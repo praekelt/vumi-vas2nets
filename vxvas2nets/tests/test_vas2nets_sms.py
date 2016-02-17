023a0e5a1c05ca8a59985e7f2f47de7d29cbb5e4
justinvdm <justinvdm.0.1.0@gmail.com>
2016-02-17 17:34:38 +0200

    Add the basics for inbound messages

--- vxvas2nets/tests/test_vas2nets_sms.py
+++ vxvas2nets/tests/test_vas2nets_sms.py
@@ -1,5 +1,128 @@
+# -*- encoding: utf-8 -*-
+
+import json
+from urllib import urlencode
+
+from twisted.internet.task import Clock
+from twisted.internet.defer import inlineCallbacks
+
 from vumi.tests.helpers import VumiTestCase
+from vumi.transports.httprpc.tests.helpers import HttpRpcTransportHelper
+from vumi.tests.utils import LogCatcher
+
+from vxvas2nets import Vas2NetsSmsTransport
 
 
 class TestVas2NetsSmsTransport(VumiTestCase):
-    pass
+    @inlineCallbacks
+    def setUp(self):
+        self.clock = Clock()
+        self.patch(Vas2NetsSmsTransport, 'get_clock', lambda _: self.clock)
+
+        self.config = {
+            'web_port': 0,
+            'web_path': '/api/v1/vas2nets/sms/',
+            'publish_status': True,
+        }
+
+        self.tx_helper = self.add_helper(
+            HttpRpcTransportHelper(Vas2NetsSmsTransport))
+
+        self.transport = yield self.tx_helper.get_transport(self.config)
+        self.transport_url = self.transport.get_transport_url(
+            self.config['web_path'])
+
+    def get_host(self):
+        addr = self.transport.web_resource.getHost()
+        return '%s:%s' % (addr.host, addr.port)
+
+    def assert_contains_items(self, obj, items):
+        for name, value in items.iteritems():
+            self.assertEqual(obj[name], value)
+
+    def assert_uri(self, actual_uri, path, params):
+        actual_path, actual_params = actual_uri.split('?')
+        self.assertEqual(actual_path, path)
+
+        self.assertEqual(
+            sorted(actual_params.split('&')),
+            sorted(urlencode(params).split('&')))
+
+    @inlineCallbacks
+    def test_inbound(self):
+        self.tx_helper.mk_request(
+            sender='+123',
+            receiver='456',
+            msgdata='hi',
+            operator='MTN',
+            recvtime='2012-02-27 19-50-07',
+            msgid='789')
+
+        [msg] = yield self.tx_helper.wait_for_dispatched_inbound(1)
+
+        self.assert_contains_items(msg, {
+            'from_addr': '+123',
+            'from_addr_type': 'msisdn',
+            'to_addr': '456',
+            'content': 'hi',
+            'provider': 'MTN',
+            'transport_metadata': {
+                'vas2nets_sms': {'msgid': '789'}
+            }
+        })
+
+    @inlineCallbacks
+    def test_inbound_decode_error(self):
+        with LogCatcher() as lc:
+            res = yield self.tx_helper.mk_request(
+                sender='+123',
+                receiver='456',
+                msgdata=u'ポケモン'.encode('utf-16'),
+                operator='MTN',
+                recvtime='2012-02-27 19-50-07',
+                msgid='789')
+
+        [error] = lc.errors[0]['message']
+        self.assertTrue("Bad request encoding" in error)
+
+        req = json.loads(res.delivered_body)['invalid_request']
+
+        self.assert_contains_items(req, {
+            'method': 'GET',
+            'path': self.config['web_path'],
+            'content': '',
+            'headers': {
+                'Connection': ['close'],
+                'Host': [self.get_host()]
+            }
+        })
+
+        self.assert_uri(req['uri'], self.config['web_path'], {
+            'sender': '+123',
+            'receiver': '456',
+            'msgdata': u'ポケモン'.encode('utf-16'),
+            'operator': 'MTN',
+            'recvtime': '2012-02-27 19-50-07',
+            'msgid': '789'
+        })
+
+    @inlineCallbacks
+    def test_inbound_bad_params(self):
+        with LogCatcher() as lc:
+            res = yield self.tx_helper.mk_request(
+                sender='+123',
+                foo='456',
+                operator='MTN',
+                recvtime='2012-02-27 19-50-07',
+                msgid='789')
+
+        [error] = lc.errors[0]['message']
+        self.assertTrue("Bad request fields for inbound message" in error)
+        self.assertTrue("foo" in error)
+        self.assertTrue("msgdata" in error)
+        self.assertTrue("receiver" in error)
+
+        self.assert_contains_items(json.loads(res.delivered_body), {
+            'unexpected_parameter': ['foo'],
+            'missing_parameter': ['msgdata', 'receiver'],
+        })
--- vxvas2nets/vas2nets_sms.py
+++ vxvas2nets/vas2nets_sms.py
@@ -1,5 +1,9 @@
+import json
+from twisted.web import http
+
 from twisted.internet.defer import inlineCallbacks
 
+from vumi import log
 from vumi.transports.httprpc import HttpRpcTransport
 
 
@@ -12,13 +16,79 @@ class Vas2NetsSmsTransport(HttpRpcTransport):
     CONFIG_CLASS = Vas2NetsSmsTransportConfig
 
     EXPECTED_FIELDS = frozenset([
+        'sender', 'receiver', 'msgdata', 'recvtime', 'msgid', 'operator'
     ])
+
+    ENCODING = 'utf-8'
 
     transport_type = 'sms'
 
+    def get_request_dict(self, request):
+        return {
+            'uri': request.uri,
+            'method': request.method,
+            'path': request.path,
+            'content': request.content.read(),
+            'headers': dict(request.requestHeaders.getAllRawHeaders()),
+        }
+
+    def get_message_dict(self, message_id, vals):
+        return {
+            'message_id': message_id,
+            'from_addr': vals['sender'],
+            'from_addr_type': 'msisdn',
+            'to_addr': vals['receiver'],
+            'content': vals['msgdata'],
+            'provider': vals['operator'],
+            'transport_type': self.transport_type,
+            'transport_metadata': {'vas2nets_sms': {'msgid': vals['msgid']}}
+        }
+
+    def respond(self, message_id, code, body=None):
+        if body is None:
+            body = {}
+
+        self.finish_request(message_id, json.dumps(body), code=code)
+
+    def handle_decode_error(self, message_id, request):
+        req = self.get_request_dict(request)
+
+        log.error('Bad request encoding: %r' % req)
+
+        self.respond(message_id, http.BAD_REQUEST, {'invalid_request': req})
+
+        # TODO publish status
+
+    def handle_bad_request_fields(self, message_id, request, errors):
+        req = self.get_request_dict(request)
+
+        log.error(
+            "Bad request fields for inbound message: %s %s"
+            % (errors, req,))
+
+        self.respond(message_id, http.BAD_REQUEST, errors)
+
+        # TODO publish status
+
+    @inlineCallbacks
+    def handle_inbound_message(self, message_id, request, vals):
+        yield self.publish_message(
+            **self.get_message_dict(message_id, vals))
+
+        self.respond(message_id, http.BAD_REQUEST, {})
+
     @inlineCallbacks
     def handle_raw_inbound_message(self, message_id, request):
-        pass
+        try:
+            vals, errors = self.get_field_values(request, self.EXPECTED_FIELDS)
+        except UnicodeDecodeError:
+            yield self.handle_decode_error(message_id, request)
+            return
+
+        if errors:
+            yield self.handle_bad_request_fields(message_id, request, errors)
+        else:
+            yield self.handle_inbound_message(message_id, request, vals)
 
     @inlineCallbacks
     def handle_outbound_message(self, message):
